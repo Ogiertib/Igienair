2c526bc523c8b3fddce98913409fa15295dac9bb
Ogier TIBERGHIEN <o.tiberghien@igienair.com>
2024-04-19 12:12:25 +0200

    ajout filtres

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,62 +1,94 @@
-import React from 'react';
-import { useState } from 'react';
-
+import React, { useState } from 'react';
 
 function HomePage() {
   const [length, setLength] = useState('');
   const [width, setWidth] = useState('');
   const [speed, setSpeed] = useState('');
   const [sum, setSum] = useState('');
+  const [sums, setSums] = useState([]);
+  const [resultFilter, setResultFilter] = useState([]);
   const [result, setResult] = useState('');
+  const [results, setResults] = useState([]);
   const [result2, setResult2] = useState('');
   const [numbers, setNumbers] = useState([]);
   const [average, setAverage] = useState('');
+  const [avgFilter, setAvgFilter] = useState([]); //  stocker les moyennes des filtres
+  const [avgFilters, setAvgFilters] = useState([]); 
   const [inputValue, setInputValue] = useState('');
 
-
   const handleChange = (e) => {
-
     const { name, value } = e.target;
     if (name === 'length') {
       setLength(value);
     } else if (name === 'width') {
       setWidth(value);
     } else if (name === 'speed') {
-      setSpeed(value);     
+      setSpeed(value);
     } else if (name === 'inputValue') {
       setInputValue(value);
-    }  
+    }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newNumbers = inputValue.split('+').map(Number); // Convertir la chaîne en tableau de nombres
+    const newNumbers = inputValue.split('+').map(Number);
     const validNumbers = newNumbers.filter(num => num !== 0);
-    const updatedNumbers = [...numbers, ...validNumbers]; // Concaténer les nouveaux nombres avec les valeurs existantes
+    const updatedNumbers = [...numbers, ...validNumbers];
     setNumbers(updatedNumbers);
-    const sum = updatedNumbers.reduce((acc, curr) => acc + curr, 0); // Calculer la somme des nombres
-    setSum(sum ? sum.toFixed(2) : null)
-    const avg = sum / updatedNumbers.length; // Calculer la moyenne
-    setAverage(avg.toFixed(2)); // Fixer le nombre de décimales à 2 et mettre à jour le state
-    setInputValue(''); // Réinitialiser la valeur de l'entrée
-    e.preventDefault();
-    const result2 = (parseFloat(length)/1000 * parseFloat(width)/1000);
-    setResult2(result2  ? result2.toFixed(2) : null);
-    const result = (( result2 * parseFloat(avg) * 3600 ));
+    const sum = updatedNumbers.reduce((acc, curr) => acc + curr, 0);
+    setSum(sum ? sum.toFixed(2) : null);
+    const avg = sum / updatedNumbers.length;
+    setAverage(avg.toFixed(2));
+    setInputValue('');
+    const result2 = (parseFloat(length) / 1000 * parseFloat(width) / 1000);
+    setResult2(result2 ? result2.toFixed(2) : null);
+    const result = ((result2 * parseFloat(avg) * 3600));
     setResult(result ? result.toFixed(2) : null);
-   
   };
 
   const handleDelete = (index) => {
-    const updatedNumbers = numbers.filter((_, i) => i !== index); // Supprimer la valeur à l'index donné
+    const updatedNumbers = numbers.filter((_, i) => i !== index);
     setNumbers(updatedNumbers);
-    const sum = updatedNumbers.reduce((acc, curr) => acc + curr, 0); // Recalculer la somme
-    setSum(sum ? sum.toFixed(2) : null)
-    const avg = updatedNumbers.length > 0 ? sum / updatedNumbers.length : null; // Recalculer la moyenne
-    setAverage(avg ? avg.toFixed(2) : null); // Fixer le nombre de décimales à 2 et mettre à jour le state de la moyenne
-    const result = updatedNumbers.reduce((acc, curr) => acc + curr, 0); // Recalculer la la vitesse d'air
-    setResult(result ? result.toFixed(2) : null)
+    const sum = updatedNumbers.reduce((acc, curr) => acc + curr, 0);
+    setSum(sum ? sum.toFixed(2) : null);
+    const avg = updatedNumbers.length > 0 ? sum / updatedNumbers.length : null;
+    setAverage(avg ? avg.toFixed(2) : null);
+    const result = updatedNumbers.reduce((acc, curr) => acc + curr, 0);
+    setResult(result ? result.toFixed(2) : null);
   };
+
+  const handleDeleteFilter = (index) => {
+    const updatedResults = [...results];
+    updatedResults.splice(index, 1);
+    setResults(updatedResults);
+    const updatedAvgFilter = [...avgFilter];
+    updatedAvgFilter.splice(index, 1); // Supprimer la moyenne associée au filtre
+    setAvgFilter(updatedAvgFilter);
+    const updatedSums = updatedResults.reduce((acc, curr) => acc + curr, 0);
+    setSums(updatedSums ? updatedSums.toFixed(2) : null);
+    setResultFilter([]);
+    
+  };
+
+  const calculateFilter = () => {
+    const updatedResults = [...results, parseFloat(result)];
+    setResults(updatedResults);
+    const updatedSums = updatedResults.reduce((acc, curr) => acc + curr, 0);
+    setSums(updatedSums ? updatedSums.toFixed(2) : null);
+    const updatedAvgFilter = [...avgFilter, parseFloat(average)]; // Ajouter la moyenne du filtre
+    const updatedAvgFilters = updatedAvgFilter.reduce((acc, curr) => acc + curr, 0) / updatedAvgFilter.length;
+    setAvgFilters(updatedAvgFilters); // Calculer et mettre à jour la moyenne des vitesses des filtres
+    setAvgFilter(updatedAvgFilter);
+    setResultFilter([]);
+  };
+  const DeleteFilter = () => {
+    setResult('');
+    setResults([])
+    setSums('')
+    setAvgFilters('')
+    setAvgFilter('')
+  };
+ 
   const handleReset = () => {
     setNumbers([]);
     setAverage('');
@@ -67,25 +99,23 @@ function HomePage() {
   return (
     <div>
       <h2>Calcul de filtre</h2>
-      {sum  && (
+      {sum && (
         <div>
-          <button onClick={() => handleReset()}>Reset</button>
-          </div>
-         )}
-      <ul> 
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      )}
+      <ul>
         {numbers.map((number, index) => (
           <li key={index}>
             <p style={{ fontWeight: 'bold' }}>{number} <button onClick={() => handleDelete(index)}>Supprimer</button></p>
           </li>
         ))}
-           
       </ul>
-   
-      
+
       <form onSubmit={handleSubmit}>
         <div>
           <label>
-            Longueur (en milimètre):
+            Longueur (en millimètre):
             <input
               type="number"
               name="length"
@@ -96,7 +126,7 @@ function HomePage() {
         </div>
         <div>
           <label>
-            Largeur (en milimètre):
+            Largeur (en millimètre):
             <input
               type="number"
               name="width"
@@ -107,7 +137,7 @@ function HomePage() {
         </div>
         <div>
           <label>
-            Entrez les vitesses d'air séparés par des + :
+            Entrez les vitesses d'air séparées par des + :
             <input
               type="text"
               name='inputValue'
@@ -119,17 +149,41 @@ function HomePage() {
         <button type="submit">Calculer</button>
       </form>
       <div>
-        {result  && (
+        {result && (
           <p>La vitesse d'air est: {result} mètres^3/h</p>
         )}
-        {result2  && (
+        {result2 && (
           <p>Le filtre fait : {result2} m2</p>
         )}
       </div>
       {average && (
         <div>
-          <p>La moyenne est :{average}</p>
-          <p>La somme est : {sum}</p>
+          <div>
+            <p>La moyenne est :{average}</p>
+            <p>La somme est : {sum}</p>
+          </div>
+          <div>
+            <button onClick={calculateFilter}>Enregistrer le filtre</button>
+            {sums && <button onClick={DeleteFilter}>Reset des filtres</button>}
+            <ul>
+              {results.map((resultFilter, index) => (
+              <li key={index}>
+                <p style={{ fontWeight: 'bold' }}>
+                  Filtre {index + 1} : {resultFilter} mètres^3/h
+                  {' '}
+                  <span style={{ fontStyle: 'italic' }}>
+                      (Vitesse moyenne : {avgFilter[index]} m/s)
+                  </span>
+                  <button onClick={() => handleDeleteFilter(index)}>Supprimer</button>
+                </p>
+              </li>
+              ))}
+            </ul>
+            {average && <div><p>Moyenne des vitesses d'air : {avgFilters} mètres^3/h</p>
+              <p>Somme des vitesses d'air : {sums}</p>
+            </div>
+            }
+          </div>
         </div>
       )}
     </div>
